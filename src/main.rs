05b7d376b33b2b951ce172a7ba752580c6d9425a
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-02-01 15:52:32 -0300

    feat: copy file to output directory

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,47 @@
+use chrono::{DateTime, Datelike, Local};
 use std::env;
 use std::fs;
-use walkdir::WalkDir;
-use chrono::{DateTime, Local, Datelike};
 use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+  let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 || args[1] != "-d" {
-        println!("Use: cargo run -- -d <directory path>");
-        return;
-    }
+  if args.len() != 3 || args[1] != "-d" {
+    println!("Use: cargo run -- -d <directory path>");
+    return;
+  }
 
-    let dir_path = &args[2];
+  let dir_path = &args[2];
 
-    if let Err(err) = match list_files_in_directory(dir_path) {
-        Ok(paths) => {
-            for path in paths {
-              //TODO: Create output destination if doenst exists
-              //TODO: Save File in the output destination
-              match get_image_modification_date(&path.to_string_lossy()) {
-                Ok(modification_date) => {
-                  println!("{}       Year: {}", path.display(), modification_date);
-                },
-                Err(err) => eprintln!("Error to obtain modification date: {}", err),
-              }
+  if let Err(err) = match list_files_in_directory(dir_path) {
+    Ok(paths) => {
+      for path in paths {
+        match get_image_modification_date(&path.to_string_lossy()) {
+          Ok(modification_year) => {
+            let output_dir = PathBuf::from("output").join(format!("{}", modification_year));
+            if let Err(err) = fs::create_dir_all(&output_dir) {
+              println!("Error in create output directory: {}", err);
+              return;
             }
-            Ok(())
+
+            let output_file = output_dir.join(path.file_name().unwrap());
+
+            if let Err(err) = fs::copy(&path, &output_file) {
+              println!("Error to copy file: {}", err);
+            } else {
+              println!("File copied: {:?}", output_file);
+            }
+          }
+          Err(err) => eprintln!("Error to obtain modification date: {}", err),
         }
-        Err(err) => Err(format!("Error: {}", err)),
-    } {
-        println!("{}", err);
+      }
+      Ok(())
     }
+    Err(err) => Err(format!("Error: {}", err)),
+  } {
+    println!("{}", err);
+  }
 }
 
 fn get_image_modification_date(path: &str) -> Result<i32, std::io::Error> {
@@ -39,27 +49,27 @@ fn get_image_modification_date(path: &str) -> Result<i32, std::io::Error> {
 
   let modification_date_in_system_time = fs::metadata(path)?.modified()?;
   let modification_date = DateTime::<Local>::from(modification_date_in_system_time);
-            
+
   let year = modification_date.year();
   Ok(year)
 }
 
 fn list_files_in_directory(dir_path: &str) -> Result<Vec<PathBuf>, String> {
-    let mut file_paths = Vec::new();
+  let mut file_paths = Vec::new();
 
-    for entry in WalkDir::new(dir_path).follow_links(true) {
-        match entry {
-            Ok(entry) => {
-              //TODO: Consider whether the file is not hidden
-                if entry.file_type().is_file() {
-                    file_paths.push(entry.path().to_path_buf());
-                }
-            }
-            Err(err) => {
-                return Err(format!("Error to list files: {}", err));
-            }
+  for entry in WalkDir::new(dir_path).follow_links(true) {
+    match entry {
+      Ok(entry) => {
+        //TODO: Consider whether the file is not hidden
+        if entry.file_type().is_file() {
+          file_paths.push(entry.path().to_path_buf());
         }
+      }
+      Err(err) => {
+        return Err(format!("Error to list files: {}", err));
+      }
     }
+  }
 
-    Ok(file_paths)
+  Ok(file_paths)
 }

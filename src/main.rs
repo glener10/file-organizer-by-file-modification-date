0358e768578d6397836e6a6c886bff80bad6b131
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-02-01 15:34:21 -0300

    feat: updatind implementation

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,49 @@
 use std::env;
+use std::path::PathBuf;
 use walkdir::WalkDir;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 || args[1] != "-d" {
-        println!("Uso: cargo run -- -d <caminho_do_diretorio>");
+        println!("Use: cargo run -- -d <directory path>");
         return;
     }
 
     let dir_path = &args[2];
 
-    if let Err(err) = list_files_in_directory(dir_path) {
-        println!("Erro: {}", err);
+    if let Err(err) = match list_files_in_directory(dir_path) {
+        Ok(paths) => {
+            for path in paths {
+              //TODO: Take modification date of path file
+              //TODO: Create output destination if doenst exists
+              //TODO: Save File in the output destination
+                println!("{}", path.display());
+            }
+            Ok(())
+        }
+        Err(err) => Err(format!("Error: {}", err)),
+    } {
+        println!("{}", err);
     }
 }
 
-fn list_files_in_directory(dir_path: &str) -> Result<(), String> {
+fn list_files_in_directory(dir_path: &str) -> Result<Vec<PathBuf>, String> {
+    let mut file_paths = Vec::new();
+
     for entry in WalkDir::new(dir_path).follow_links(true) {
         match entry {
             Ok(entry) => {
+              //TODO: Consider whether the file is not hidden
                 if entry.file_type().is_file() {
-                    println!("{}", entry.path().display());
+                    file_paths.push(entry.path().to_path_buf());
                 }
             }
             Err(err) => {
-                return Err(format!("Erro ao listar arquivos: {}", err));
+                return Err(format!("Error to list files: {}", err));
             }
         }
     }
 
-    Ok(())
+    Ok(file_paths)
 }

dad26a2c1b1efd7b4a87e756ac9eaec20f1c570e
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-02-02 15:30:05 -0300

    feat: logging of files with repeated name

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use clap::{App, Arg, ArgMatches};
 use rand::Rng;
 use std::collections::HashSet;
 use std::ffi::{OsStr, OsString};
-use std::fs;
+use std::fs::{self, File};
+use std::io::Write;
 use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
@@ -41,7 +42,9 @@ fn main() -> Result<(), AppError> {
   let paths = list_files_in_directory(dir_path)?;
   let paths_len = paths.len();
   let mut count = 0;
+  let mut count_files_with_same_name = 0;
   let mut files_transfered = HashSet::<String>::new();
+  let mut files_with_repeat_name: Vec<String> = Vec::new();
 
   println!("Organizing Files...");
   for path in paths {
@@ -62,11 +65,12 @@ fn main() -> Result<(), AppError> {
     let new_name_with_random_id: OsString;
 
     if !files_transfered.insert(file_name_str.clone()) {
-      //TODO: Logging .txt with all files have same name
+      count_files_with_same_name += 1;
       let mut rng = rand::thread_rng();
       let random_id: u32 = rng.gen();
       new_name_with_random_id = OsString::from(format!("{}_{}", random_id, file_name_str));
       file_name = OsStr::new(&new_name_with_random_id);
+      files_with_repeat_name.push(file_name.to_string_lossy().to_string());
       files_transfered.insert(file_name.to_string_lossy().to_string());
     }
 
@@ -82,6 +86,19 @@ fn main() -> Result<(), AppError> {
     println!(
       "{}% Concluded, File {} of {}  -  File Name: {:?}",
       percent, count, paths_len, file_name
+    );
+  }
+  if count_files_with_same_name > 0 {
+    let output_file_path = format!("{}/filesWithRepeatedName.txt", outputh_directory);
+    let output_file = File::create(&output_file_path)?;
+
+    for result in &files_with_repeat_name {
+      writeln!(&output_file, "{}", result)?;
+    }
+
+    println!(
+      "Total of {} file with the repeated name",
+      count_files_with_same_name
     );
   }
 

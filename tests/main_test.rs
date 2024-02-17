705dfb9b252012a0963433c58094ace166eef18c
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-02-17 09:14:34 -0300

    test: add cleaning output files after execution of main program

--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,4 +1,9 @@
+use std::fs;
 use std::process::Command;
+
+fn clean_output_files() {
+  fs::remove_dir_all("output").expect("Fail to execute the command 'rm -r output'");
+}
 
 #[test]
 fn test_main_with_args() {
@@ -8,10 +13,12 @@ fn test_main_with_args() {
     .arg("-d")
     .arg("./tests/inputsForTest/")
     .output()
-    .expect("");
+    .expect("Fail to execute the command of main program");
 
   assert!(output.status.success());
   let stdout = String::from_utf8(output.stdout).expect("Saída não válida UTF-8");
   let expected_output = "Total of 1 file with the repeated name\n\nTotal of 4 files with '.png' extension\nTotal of 1 files with '.mp4' extension\n\n\nTotal of 5 files organized\n"; // Substitua esta string pela saída esperada do seu programa
   assert!(stdout.contains(expected_output));
+
+  clean_output_files();
 }
